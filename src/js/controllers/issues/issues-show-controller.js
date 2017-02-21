4610f60140759f22b295e7ad416a15df628e428e
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-21 13:57:18 +0000

    Started the logic for adding messages

--- src/js/controllers/issues/issues-show-controller.js
+++ src/js/controllers/issues/issues-show-controller.js
@@ -7,10 +7,22 @@ function IssuesShowCtrl($stateParams, Issue) {
   const vm = this;
 
   Issue
-    .get({id: $stateParams.id})
-    .$promise
-    .then(response => {
-      vm.issue = response;
-    });
+  .get({id: $stateParams.id})
+  .$promise
+  .then(response => {
+    vm.issue = response;
+  });
+
+  vm.AddMessage = function(issueId) {
+    console.log('clicked AddMessage button for', issueId);
+  };
+
+  vm.EditMessage = function(messageID) {
+    console.log('clicked EditMessage button for', messageID);
+  };
+
+  vm.DeleteMessage = function(messageID) {
+    console.log('clicked Delete Message button for', messageID);
+  };
 
 }
