b44b2f6a528303db9ad2c11cffb57ef42e375d37
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-22 22:28:43 +0000

    reworked the Action Cable logic so its easier to read

--- src/js/controllers/issues/issues-show-controller.js
+++ src/js/controllers/issues/issues-show-controller.js
@@ -8,22 +8,6 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
   const vm = this;
 
   const consumer = new ActionCableChannel('IssuesChannel', { id: $stateParams.id });
-
-  function callback(message) {
-    console.log('RECEIVED', message)
-    vm.issue.messages.push({
-      msg_text: message
-    });
-  }
-  consumer.subscribe(callback).then(function(){
-    vm.sendMessage = function(message){
-      consumer.send(message, 'new_message');
-    };
-    // $scope.$on("$destroy", function(){
-    //   consumer.unsubscribe().then(function(){ $scope.sendToMyChannel = undefined; });
-    // });
-  });
-
   vm.status = ActionCableSocketWrangler;
 
   vm.currentUser = CurrentUserService.currentUser;
@@ -44,23 +28,31 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
     if (vm.newMessage.msg_text === undefined) {
       //do nothing, as no text has been entered
     } else {
-      vm.newMessage.issue_id = vm.issue.id;
-      vm.newMessage.sender_id = vm.currentUser.id;
-      vm.newMessage.receiver_id = vm.issue.user.id;
-
-      Message
-      .save(vm.newMessage)
-      .$promise
-      .then((response) => {
-        vm.sendMessage(vm.newMessage.msg_text);
-        vm.newMessage.msg_text = null;
-        console.log('THIS IS THE ISSUE AFTER SAVE', vm.issue);
-
-      }, err => {
-        console.log(err);
-      });
+      vm.sendMessage(vm.newMessage.msg_text);
     }
   };
+
+  consumer.subscribe(SaveMessage).then(function() {
+    vm.sendMessage = function(message){
+      consumer.send(message, 'new_message');
+      vm.newMessage.msg_text = null;
+    };
+    // $scope.$on("$destroy", function(){
+    //   consumer.unsubscribe().then(function(){ $scope.sendToMyChannel = undefined; });
+    // });
+  });
+
+  function SaveMessage(message) {
+    vm.newMessage.issue_id = vm.issue.id;
+    vm.newMessage.sender_id = vm.currentUser.id;
+    vm.newMessage.receiver_id = vm.issue.user.id;
+    vm.newMessage.msg_text = message;
+
+    Message
+    .save(vm.newMessage).$promise.then((response) => {
+      console.log('LOGGING RESPONSE', response);
+    });
+  }
 
   vm.ShowEditField = function(id) {
     vm.editing = id;
