f69ef7d609a1f19393b0c7af06cfd8b9a91cca4d
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-22 10:40:39 +0000

    fixed issue around first message and blank message

--- src/js/controllers/issues/issues-show-controller.js
+++ src/js/controllers/issues/issues-show-controller.js
@@ -2,12 +2,12 @@ angular
 .module('letsTalk')
 .controller('issuesShowCtrl', IssuesShowCtrl);
 
-IssuesShowCtrl.$inject = ['$stateParams', 'Issue', 'CurrentUserService', 'Message', '$state', '$scope'];
-function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state, $scope ) {
+IssuesShowCtrl.$inject = ['$stateParams', 'Issue', 'CurrentUserService', 'Message', '$state'];
+function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state) {
   const vm = this;
 
   vm.currentUser = CurrentUserService.currentUser;
-  vm.newMessage;
+  vm.newMessage = {};
 
   Issue
   .get({id: $stateParams.id})
@@ -21,29 +21,33 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
   });
 
   vm.AddMessage = function() {
-    vm.newMessage.issue_id = vm.issue.id;
-    vm.newMessage.sender_id = vm.currentUser.id;
-    vm.newMessage.receiver_id = vm.issue.user.id;
+    if (vm.newMessage.msg_text === undefined) {
+      //do nothing, as no text has been entered
+    } else {
+      vm.newMessage.issue_id = vm.issue.id;
+      vm.newMessage.sender_id = vm.currentUser.id;
+      vm.newMessage.receiver_id = vm.issue.user.id;
 
-    Message
-    .save(vm.newMessage)
-    .$promise
-    .then((response) => {
-      vm.newMessage.msg_text = null;
-      Issue
-      .get({id: response.issue_id})
+      Message
+      .save(vm.newMessage)
       .$promise
-      .then(response => {
-        vm.temp = response;
-        vm.temp.messages.sort(function(a, b){
-          return a.id-b.id;
+      .then((response) => {
+        vm.newMessage.msg_text = null;
+        Issue
+        .get({id: response.issue_id})
+        .$promise
+        .then(response => {
+          vm.temp = response;
+          vm.temp.messages.sort(function(a, b){
+            return a.id-b.id;
+          });
+          vm.issue = vm.temp;
+          $state.go('issuesShow', {id: vm.issue.id});
         });
-        vm.issue = vm.temp;
-        $state.go('issuesShow', {id: vm.issue.id});
+      }, err => {
+        console.log(err);
       });
-    }, err => {
-      console.log(err);
-    });
+    }
   };
 
   vm.ShowEditField = function() {
