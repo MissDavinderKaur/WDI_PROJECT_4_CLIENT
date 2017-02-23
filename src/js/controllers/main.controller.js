6721e035c260b2e4958a700f639759a397a2974d
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-23 15:55:44 +0000

    Home, register and login pages have been styled

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -2,8 +2,8 @@ angular
   .module('letsTalk')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$rootScope','CurrentUserService', '$state'];
-function MainCtrl($rootScope, CurrentUserService, $state){
+MainCtrl.$inject = ['$rootScope','CurrentUserService', '$state', '$document'];
+function MainCtrl($rootScope, CurrentUserService, $state, $document){
   const vm = this;
 
   $rootScope.$on('loggedIn', () => {
@@ -20,4 +20,51 @@ function MainCtrl($rootScope, CurrentUserService, $state){
     vm.user = null;
     $state.go('home');
   });
+
+  // (function (window, document) {
+  //
+  //   var layout = document.getElementById('layout'),
+  //   menu     = document.getElementById('menu'),
+  //   menuLink = document.getElementById('menuLink'),
+  //   content  = document.getElementById('main');
+  //
+  //   function toggleClass(element, className) {
+  //     var classes = element.className.split(/\s+/),
+  //     length = classes.length,
+  //     i = 0;
+  //
+  //     for(; i < length; i++) {
+  //       if (classes[i] === className) {
+  //         classes.splice(i, 1);
+  //         break;
+  //       }
+  //     }
+  //     // The className is not found
+  //     if (length === classes.length) {
+  //       classes.push(className);
+  //     }
+  //
+  //     element.className = classes.join(' ');
+  //   }
+  //
+  //   function toggleAll(e) {
+  //     var active = 'active';
+  //
+  //     e.preventDefault();
+  //     toggleClass(layout, active);
+  //     toggleClass(menu, active);
+  //     toggleClass(menuLink, active);
+  //   }
+  //
+  //   menuLink.onclick = function (e) {
+  //     toggleAll(e);
+  //   };
+  //
+  //   content.onclick = function(e) {
+  //     if (menu.className.indexOf('active') !== -1) {
+  //       toggleAll(e);
+  //     }
+  //   };
+  //
+  // }(this, this.document));
 }
