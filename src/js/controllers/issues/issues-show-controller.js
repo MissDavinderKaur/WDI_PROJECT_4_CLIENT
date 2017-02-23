34c593dd58bd805f9f4da36ea9d61dcbac057502
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-02-23 12:12:59 +0000

    The IM fucntion works

--- src/js/controllers/issues/issues-show-controller.js
+++ src/js/controllers/issues/issues-show-controller.js
@@ -12,21 +12,18 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
 
   const consumer = new ActionCableChannel('IssuesChannel', { id: $stateParams.id });
   function callback(message) {
-    console.log('RECEIVED', message)
     vm.issue.messages.push({
       msg_text: message
     });
+    // vm.issue.messages.push(vm.newMessage);
   }
   consumer.subscribe(callback).then(function(){
     vm.sendMessage = function(message){
       consumer.send(message, 'new_message');
     };
-    // $scope.$on("$destroy", function(){
-    //   consumer.unsubscribe().then(function(){ $scope.sendToMyChannel = undefined; });
-    // });
   });
-  vm.status = ActionCableSocketWrangler;
 
+  vm.status = ActionCableSocketWrangler;
 
   Issue
   .get({id: $stateParams.id})
@@ -43,6 +40,7 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
     if (vm.newMessage.msg_text === undefined) {
       //do nothing, as no text has been entered
     } else {
+      //as msg_text is modelled from the front end, the newMessage object can be saved
       vm.newMessage.issue_id = vm.issue.id;
       vm.newMessage.sender_id = vm.currentUser.id;
       vm.newMessage.receiver_id = vm.issue.user.id;
@@ -50,21 +48,9 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
       Message
       .save(vm.newMessage)
       .$promise
-      .then((response) => {
-        console.log('THIS IS THE RESPONSE FROM THE MESSAGE SAVE', response);
+      .then(() => {
         vm.sendMessage(vm.newMessage.msg_text);
         vm.newMessage.msg_text = null;
-        // Issue
-        // .get({id: response.issue_id})
-        // .$promise
-        // .then(response => {
-        //   vm.temp = response;
-        //   vm.temp.messages.sort(function(a, b){
-        //     return a.id-b.id;
-        //   });
-        //   vm.issue = vm.temp;
-        //   $state.go('issuesShow', {id: vm.issue.id});
-        // });
       }, err => {
         console.log(err);
       });
@@ -114,4 +100,5 @@ function IssuesShowCtrl($stateParams, Issue, CurrentUserService, Message, $state
       console.log(err);
     });
   };
+
 }
